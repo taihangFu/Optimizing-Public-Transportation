9440df1a4bcfc8dcf5684c085a5bc0dbc971f388
taihangFu <tatsonreload@icloud.com>
2020-01-14 09:21:38 +0000

    defined value schema for turnstile_value

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -13,13 +13,7 @@ logger = logging.getLogger(__name__)
 
 class Turnstile(Producer):
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
-
-    #
-    # TODO: Define this value schema in `schemas/turnstile_value.json, then uncomment the below
-    #
-    #value_schema = avro.load(
-    #    f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
-    #)
+    value_schema = avro.load(f{Path(__file__).parents[0]}/schemas/turnstile_value.json")
 
     def __init__(self, station):
         """Create the Turnstile"""
