b2551729ad230fd431cf93731d1c68bf8d0ecd2b
taihangFu <tatsonreload@icloud.com>
2020-01-27 08:56:13 +0000

    updated toic name for turnstile

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -26,7 +26,7 @@ class Turnstile(Producer):
         )
                              
         super().__init__(
-            f"{station_name}", # TODO: Come up with a better topic name
+            f"com.udacity.station.turnstile.v1", # TODO: Come up with a better topic name
             key_schema=Turnstile.key_schema,
             value_schema=Turnstile.value_schema,
             num_partitions=4, #TODO: Partitions = Max(Overall Throughput/Producer Throughput, Overall Throughput/Consumer Throughput) 
